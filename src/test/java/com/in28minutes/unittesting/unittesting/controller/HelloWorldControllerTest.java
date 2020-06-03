b151eee3166c86369b7ba2e5bacfeaf54fc7d99f
Joaquin Luque <luque_joaquin@cat.com>
2020-06-03 14:02:47 -0300

    Updated test for JUnit5

--- src/test/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
@@ -3,17 +3,18 @@ package com.in28minutes.unittesting.unittesting.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class) Note: JUnit4
+@ExtendWith(SpringExtension.class) // Note: JUnit5 for backwards compatibility, can be removed
 @WebMvcTest(HelloWorldController.class)
 class HelloWorldControllerTest {
 
