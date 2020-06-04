2e895c7cd3fe078a526bacf4c63de194e15ccfaf
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2020-06-04 15:31:23 -0300

    Using @MockBean to mock out dependencies you do not want to talk

--- src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerIT.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerIT.java
@@ -1,5 +1,10 @@
 package com.in28minutes.unittesting.unittesting.controller;
 
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,8 +12,12 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.in28minutes.unittesting.unittesting.data.ItemRepository;
+import com.in28minutes.unittesting.unittesting.model.Item;
 
 // Launch a Spring context
 //@RunWith(SpringRunner.class) Note: JUnit4
@@ -24,8 +33,20 @@ class ItemControllerIT {
     @Autowired
     private TestRestTemplate restTemplate;
     
+    @MockBean
+    ItemRepository repository;
+    
     @Test
     public void contextLoads() throws JSONException {
+        /*
+         * With this and @MockBean we are mocking the repository instead of using the in memory database
+         */
+        when(repository.findAll()).thenReturn(Arrays.asList(
+                new Item(10001, "Item2", 10, 20),
+                new Item(10002, "Item2", 10, 20),
+                new Item(10003, "Item2", 10, 20)
+                ));
+        
         String response = this.restTemplate.getForObject("/all-items-from-database", String.class);
         JSONAssert.assertEquals("[{id:10001,value:200},{id:10002},{id:10003}]", response, false);
     }
