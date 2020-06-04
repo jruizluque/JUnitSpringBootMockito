06103f61c1a826221b2fb54b8315c22cc10fbc08
Joaquin Luque <luque_joaquin@cat.com>
2020-06-04 15:41:34 -0300

    Creating Different Test Configuration

--- src/test/java/com/in28minutes/unittesting/unittesting/UnitTestingApplicationTests.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/UnitTestingApplicationTests.java
@@ -2,8 +2,11 @@ package com.in28minutes.unittesting.unittesting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+/* The properties file specified here has the highest priority, it overrides the others */
+@TestPropertySource(locations= {"classpath:test-configuration.properties"})
 class UnitTestingApplicationTests {
 
 	@Test
